e2c8cb6e5b9e7db6187c845c5fdc4a3ae66851b1
kyorohiro <kyorohiro@gmail.com>
2013-07-29 00:31:25 +0900

    .

--- Hetimatan/src/net/hetimatan/net/torrent/krpc/ResponsePing.java
+++ Hetimatan/src/net/hetimatan/net/torrent/krpc/ResponsePing.java
@@ -7,24 +7,24 @@ import net.hetimatan.net.torrent.util.bencode.BenDiction;
 import net.hetimatan.net.torrent.util.bencode.BenObject;
 import net.hetimatan.net.torrent.util.bencode.BenString;
 
-public class RequestPing {
+public class ResponsePing {
 
 	private String mId;
 	private String mTransactionId = "xx";
 
-	public RequestPing(String id, String transactionId) {
+	public ResponsePing(String id, String transactionId) {
 		mId = id;
 		mTransactionId = transactionId;
 	} 
 
-	public RequestPing decode(MarkableReader reader) throws IOException {
+	public ResponsePing decode(MarkableReader reader) throws IOException {
 		reader.popMark();
-		String transactionid = "xx";
+		String transactionid = "";
 		try {
 			BenDiction diction = BenDiction.decodeDiction(reader);
 			{
 				BenObject v = diction.getBenValue("y");
-				if(v.getType() == BenObject.TYPE_STRI && "q".equals(v.toString())) {
+				if(v.getType() == BenObject.TYPE_STRI && "r".equals(v.toString())) {
 				} else {
 					reader.backToMark();
 					throw new IOException();
@@ -47,6 +47,7 @@ public class RequestPing {
 					throw new IOException();
 				}
 			}
+
 			BenObject a = diction.getBenValue("a");
 			if(a.getType() != BenObject.TYPE_DICT) {
 				reader.backToMark();
@@ -57,7 +58,7 @@ public class RequestPing {
 				reader.backToMark();
 				throw new IOException();
 			}			
-			return new RequestPing(id.toString(), transactionid);
+			return new ResponsePing(id.toString(), transactionid);
 		} finally {
 			reader.pushMark();
 		}
@@ -65,9 +66,8 @@ public class RequestPing {
 
 	public void encode(OutputStream output) throws IOException {
 		BenDiction diction = new BenDiction();
+		diction.append("y", new BenString("r"));
 		diction.append("t", new BenString(mTransactionId));
-		diction.append("y", new BenString("q"));
-		diction.append("q", new BenString("ping"));
 		BenDiction a = new BenDiction();
 		a.append("id", new BenString(mId));
 		diction.append("a", a);
