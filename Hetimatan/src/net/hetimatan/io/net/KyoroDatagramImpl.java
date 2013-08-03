d948883fd140fd1396816679a52a616db6260332
kyorohiro <kyorohiro@gmail.com>
2013-08-03 21:51:58 +0900

    now adding datagramsocket

--- Hetimatan/src/net/hetimatan/io/net/KyoroDatagramImpl.java
+++ Hetimatan/src/net/hetimatan/io/net/KyoroDatagramImpl.java
@@ -1,12 +1,21 @@
 package net.hetimatan.io.net;
 
+import java.io.IOException;
+import java.nio.channels.DatagramChannel;
 import java.nio.channels.SelectableChannel;
 
 public class KyoroDatagramImpl extends KyoroSelectable {
 
+	private DatagramChannel mChannel = null;
+
+	public KyoroDatagramImpl() throws IOException {
+		mChannel = DatagramChannel.open();
+		mChannel.configureBlocking(false);
+	}
+
 	@Override
 	public SelectableChannel getRawChannel() {
-		return null;
+		return mChannel;
 	}
 
 }
