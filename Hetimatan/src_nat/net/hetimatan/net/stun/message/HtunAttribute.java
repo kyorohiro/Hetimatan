d689aa5374b1b56e77ecbd45fd4356678cb9f0d4
kyorohiro <kyorohiro@gmail.com>
2014-01-13 14:17:38 +0900

    ..

--- Hetimatan/src_nat/net/hetimatan/net/stun/message/HtunAttribute.java
+++ Hetimatan/src_nat/net/hetimatan/net/stun/message/HtunAttribute.java
@@ -13,4 +13,22 @@ public class HtunAttribute {
 		
 		return null;
 	}
+
+	public static class HtunChangeRequest extends HtunAttribute {
+		
+		public static HtunChangeRequest decode(MarkableFileReader reader) throws IOException {
+			int type = MarkableReaderHelper.readShort(reader, ByteArrayBuilder.BYTEORDER_BIG_ENDIAN);
+			int status = reader.read();
+			boolean changePort = false;
+			boolean changeIp = false;
+			if((status&0x01) == 0x01) {
+				changePort = true;
+			}
+			else if((status&0x04) == 0x04) {
+				changeIp = true;
+			}
+			return null;
+		}
+	}
 }
+
