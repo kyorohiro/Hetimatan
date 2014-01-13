b8821f24abc25d17b4c82014dff92a4f890b59c7
kyorohiro <kyorohiro@gmail.com>
2014-01-14 00:19:02 +0900

    ..

--- Hetimatan/src_nat/net/hetimatan/net/stun/message/HtunHeader.java
+++ Hetimatan/src_nat/net/hetimatan/net/stun/message/HtunHeader.java
@@ -1,9 +1,14 @@
 package net.hetimatan.net.stun.message;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.util.LinkedList;
 
+import net.hetimatan.io.file.KyoroFile;
 import net.hetimatan.io.file.MarkableFileReader;
 import net.hetimatan.io.file.MarkableReaderHelper;
+import net.hetimatan.io.filen.CashKyoroFile;
+import net.hetimatan.io.filen.CashKyoroFileHelper;
 import net.hetimatan.util.io.ByteArrayBuilder;
 
 public class HtunHeader {
@@ -14,6 +19,52 @@ public class HtunHeader {
 	public static final int SHARED_SECRET_RESPONSE = 0x102;
 	public static final int SHARED_SECRET_ERROR_RESPONSE = 0x112;
 
+	private int mType = 0;
+	private byte[] mId = new byte[16];
+	private LinkedList<HtunAttribute> mAttributes = new LinkedList<>();
+
+	public HtunHeader(int requestType, byte[] id) {
+		mType = requestType;
+		for(int i=0;i<16;i++) {
+			mId[i] = id[i];
+		}
+	}
+
+	public int getType() {
+		return mType;
+	}
+
+	public byte[] getId() {
+		return mId;
+	}
+
+	public void addAttribute(HtunAttribute attribute) {
+		mAttributes.add(attribute);
+	}
+
+	public void encode(OutputStream output) throws IOException {
+		// 2byte zero sign 
+		output.write(ByteArrayBuilder.parseShort(0, ByteArrayBuilder.BYTEORDER_BIG_ENDIAN));
+
+		// 2byte message type 
+		output.write(ByteArrayBuilder.parseShort(mType, ByteArrayBuilder.BYTEORDER_BIG_ENDIAN));
+
+		// create body 
+		CashKyoroFile tmp = new CashKyoroFile(1024);
+		for(int i=0;i<mAttributes.size();i++) {
+			HtunAttribute attribute = mAttributes.get(i);
+			attribute.encode(tmp.getLastOutput());
+		}
+		
+		// 2byte message length 
+		output.write(ByteArrayBuilder.parseShort((int)tmp.length(), ByteArrayBuilder.BYTEORDER_BIG_ENDIAN));
+
+		output.write(mId);
+		// nbyte messae body
+		output.write(CashKyoroFileHelper.newBinary(tmp));
+
+	}
+
 	public static HtunHeader decode(MarkableFileReader reader) throws IOException {
 		// 2byte zero sign.  
 		int sign = MarkableReaderHelper.readShort(reader, ByteArrayBuilder.BYTEORDER_BIG_ENDIAN);
@@ -22,18 +73,27 @@ public class HtunHeader {
 		// 2byte message type
 		int messageType = MarkableReaderHelper.readShort(reader, ByteArrayBuilder.BYTEORDER_BIG_ENDIAN);
 		
-		// 2byte message length
+		// 2byte message body length
 		int messageLength = MarkableReaderHelper.readShort(reader, ByteArrayBuilder.BYTEORDER_BIG_ENDIAN);
 		if(messageLength<0) {
 			throw new IOException("bad messageLength len="+messageLength );			
 		}
-		// data
-		byte[] content = new byte[messageLength];
-		int readed = reader.read(content);
-		if(messageLength != readed) {
-			throw new IOException("messageLength exp="+messageLength+",ret="+readed );
+		
+		// id length
+		byte[] id = new byte[16];
+		int ret = reader.read(id);
+		if (ret != 16) {
+			throw new IOException("bad id len="+ret);
 		}
-		return null;
+
+		HtunHeader header = new HtunHeader(messageType, id);
+		// data
+		int begin = (int)reader.getFilePointer();
+		int end = begin+messageLength;
+		while(end<reader.getFilePointer()) {
+			header.addAttribute(HtunAttribute.decode(reader));
+		}
+		return header;
 	}
 
 }
--- Hetimatan/test_nat/net/hetimatan/net/stun/message/TestForHtunHeader.java
+++ Hetimatan/test_nat/net/hetimatan/net/stun/message/TestForHtunHeader.java
@@ -4,7 +4,7 @@ import junit.framework.TestCase;
 
 public class TestForHtunHeader extends TestCase {
 	public void testCreate() {
-		
-		
+		byte[] id = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+		HtunHeader header = new HtunHeader(HtunHeader.BINDING_REQUEST, id);
 	}
 }
